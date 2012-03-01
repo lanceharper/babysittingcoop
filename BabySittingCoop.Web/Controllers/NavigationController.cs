8ac4f0b7071c81020a7f74275ea517d08eee8f81
lance <lance.harper@gmail.com>
2012-02-29 23:00:12 -0600

    move nav to renderaction

---
+++ BabySittingCoop.Web/Controllers/NavigationController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BabySittingCoop.Web.Models;
+
+namespace BabySittingCoop.Web.Controllers
+{
+    public class NavigationController : Controller
+    {
+        [ChildActionOnly]
+        public ActionResult Menu(string active)
+        {
+            var navOptions = new[]
+                                 {
+                                     new NavigationOption { Text = "Rankings", ActionName = "Index", ControllerName = "Home", IsActive = "Rankings" == active },
+                                     new NavigationOption { Text = "Recommendations", ActionName = "Recommendations", ControllerName = "Home", IsActive = "Recommendations" == active }
+                                     
+                                 };
+
+            return View("Navigation", navOptions);
+        }
+
+    }
+}
---
+++ BabySittingCoop.Web/Models/NavigationOption.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BabySittingCoop.Web.Models
+{
+    public class NavigationOption
+    {
+        public string Text { get; set; }
+        public bool IsActive { get; set; }
+        public string ControllerName { get; set; }
+        public string ActionName { get; set; }
+    }
+}
